3fcd1b8f3ea454077822c14d34fa44d9f0d2902d
Arindam2003 <dindaarindam000@gmail.com>
2025-07-25 13:03:37 +0530

    card1-fixed

--- src/components/Arena.jsx
+++ src/components/Arena.jsx
@@ -23,7 +23,7 @@ const Arena = () => {
 
                     <div className="relative overflow-hidden rounded-3xl border border-white aspect-[4.4/4] transform transition-transform duration-300 hover:scale-105">
                         <div className="absolute h-full w-full">
-                            <img src="public/images/bento-card1.png" alt="card-1" />
+                            <img src="images/bento-card1.png" alt="card-1" />
                             <button className='absolute bottom-0 h-16 w-full bg-black bg-opacity-70 text-2xl font-bold hover:text-yellow-400 transition-all duration-300 text-nowrap'>
                                 <i className='bx bx-store-alt'></i> NFT STORE
                             </button>
